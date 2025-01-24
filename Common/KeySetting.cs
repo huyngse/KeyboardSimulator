fc3dba8ff3be546ae80f9a89b376f9a54277acd8
huyngse <ngogiahuy775309@gmail.com>
2025-01-24 21:11:55 +0700

    chore(set up): add basic models

---
+++ Common/KeySetting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class KeySetting
+    {
+        public int Id { get; set; }
+        public char Key { get; set; }
+        // hold || press || heat activation
+        public string Type { get; set; } = "";
+        // millisecond
+        public int Delay { get; set; }
+        public int? HoldInterval { get; set; } = 0;
+        public int? HeatCD { get; set; } = 10;
+        public int RandomIndex { get; set; } = 1;
+        public float Weight { get; set; }
+        public int SettingGroupId { get; set; }
+        public SettingGroup? SettingGroup { get; set; }
+    }
+}
---
+++ Common/SettingGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SettingGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<KeySetting> Settings { get; set; } = [];
+    }
+}
